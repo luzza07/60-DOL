b47a614d041244405b7ecbf88dfa494fc3aff118
luzza07 <luzzadangol456@gmail.com>
2025-06-10 18:41:49 +0545

    Showing a loading indicator

--- ReactQuery/src/TodoList.tsx
+++ ReactQuery/src/TodoList.tsx
@@ -4,13 +4,18 @@ import axios from "axios";
 const TodoList = () => {
   const fetchTodos = () =>
     axios
-      .get<Todo[]>("https://xjsonplaceholder.typicode.com/todos")
+      .get<Todo[]>("https://jsonplaceholder.typicode.com/todos")
       .then((res) => res.data);
-  const { data: todos, error } = useQuery<Todo[],Error>({
+  const {
+    data: todos,
+    error,
+    isLoading,
+  } = useQuery<Todo[], Error>({
     queryKey: ["todos"],
     queryFn: fetchTodos,
   });
 
+  if (isLoading) return <p>Please wait its Loading...............</p>;
   if (error) return <p>{error.message}</p>;
   return (
     <>
