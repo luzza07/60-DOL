fc599d803cc97c42597172d0af73ae245506ec71
luzza07 <luzzadangol456@gmail.com>
2025-06-11 21:31:16 +0545

    Optimistic updates

--- ReactQuery/src/TodoForm.tsx
+++ ReactQuery/src/TodoForm.tsx
@@ -3,31 +3,46 @@ import { useRef } from "react";
 import type { Todo } from "./hooks/useTodos";
 import axios from "axios";
 
+
+interface  AddTodoContext{
+    previousTodos:Todo[]
+}
 const TodoForm = () => {
   const queryClient = useQueryClient();
-  const addTodo = useMutation<Todo,Error,Todo>({
+  const addTodo = useMutation<Todo, Error, Todo,AddTodoContext>({
     mutationFn: (todo: Todo) =>
       axios
         .post<Todo>("https://jsonplaceholder.typicode.com/todos", todo)
         .then((res) => res.data),
-    onSuccess: (savedTodo, newTodo) => {
-      //   queryClient.invalidateQueries({
-      //     queryKey: ["todos"],
-      //   });
-
+    onMutate: (newTodo: Todo) => {
+       const previousTodos= queryClient.getQueryData<Todo[]>(['todos'])||[]
       queryClient.setQueryData<Todo[]>(["todos"], (todos) => [
-        savedTodo,
+        newTodo,
         ...(todos || []),
       ]);
-
-
-     if(ref.current) ref.current.value='';
+      if (ref.current) ref.current.value = "";
+      return{previousTodos};
     },
+    onSuccess: (savedTodo, newTodo) => {
+      queryClient.setQueryData<Todo[]>(["todos"], (todos) =>
+        todos?.map((todo) => (todo === newTodo ? savedTodo : todo))
+      );
+    },onError:(error,newTodo,context)=>{
+        if(!context )return;
+        queryClient.setQueryData<Todo[]>(['todos'],context.previousTodos)
+    }
   });
+
+  //   queryClient.invalidateQueries({
+  //     queryKey: ["todos"],
+  //   });
+
   const ref = useRef<HTMLInputElement>(null);
   return (
     <>
-      {addTodo.error && <div className="alert alert-danger">{addTodo.error.message}</div>}
+      {addTodo.error && (
+        <div className="alert alert-danger">{addTodo.error.message}</div>
+      )}
       <form
         onSubmit={(event) => {
           event.preventDefault();
@@ -44,7 +59,9 @@ const TodoForm = () => {
           <input ref={ref} type="text" />
         </div>
         <div>
-          <button disabled={addTodo.isPending}>{addTodo.isPending ? 'Adding...' : 'Add'}</button>
+          <button disabled={addTodo.isPending}>
+            {addTodo.isPending ? "Adding..." : "Add"}
+          </button>
         </div>
       </form>
     </>
