dc9b3775e1ed8e5ed16231d5c4475a578fed331c
luzza07 <luzzadangol456@gmail.com>
2025-06-17 23:03:49 +0545

    Getting data about current route

--- ReactRouting/src/components/UserDetailPage.tsx
+++ ReactRouting/src/components/UserDetailPage.tsx
@@ -1,6 +1,15 @@
 import React from 'react'
+import { useLocation, useParams, useSearchParams } from 'react-router-dom'
 
 const UserDetailPage = () => {
+   const params= useParams();
+  const [searchParams,setSearchParams]= useSearchParams();
+  const location=useLocation();
+  console.log(location);
+  
+  console.log(searchParams.get("age"));
+  
+   
   return (
     <div>User</div>
   )
