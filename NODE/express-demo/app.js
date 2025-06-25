7689198981ec1994d12c1f7d009b7b447bbb40e2
luzza07 <luzzadangol456@gmail.com>
2025-06-25 22:23:07 +0545

    Environment variables

--- NODE/express-demo/app.js
+++ NODE/express-demo/app.js
@@ -2,11 +2,14 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {res.send("Hello world11111111");});
+app.get("/", (req, res) => {
+  res.send("Hello world11111111");
+});
 
+app.get("/api/courses", (req, res) => {
+  res.send([1, 2, 3]);
+});
 
-app.get('/api/courses',(req,res)=>{
-    res.send([1,2,3])
-})
-
-app.listen(3000,()=> console.log("listening on port 3000"));
+//PORT
+const port=process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}........`));
